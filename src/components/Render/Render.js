4294c419fe4dfbb07b87d563bf751013cbc70599
Shivang Patel <shivang.patel02062002@gmail.com>
2021-07-28 18:01:12 +0530

    Done with statically rendering the website

--- src/components/Render/Render.js
+++ src/components/Render/Render.js
@@ -1,9 +1,7 @@
 import React from "react";
 import Card from "../Card/Card";
-import Button from "../Button/Button";
 import Type from "./Type";
 
-import classes from "../Button/Button.module.css";
 import "./Render.module.css";
 
 const Render = () => {
@@ -11,7 +9,7 @@ const Render = () => {
     <div>
       <h1>Quote Generator</h1>
       <Card>
-        <Button text="Generate Quote" className={classes.head} />
+        <label>Generate Quote On</label>
         <Type />
       </Card>
     </div>
