3d2037fc21cd5d77323926ffac97a7446973f951
Shivang Patel <shivang.patel02062002@gmail.com>
2021-07-28 11:05:20 +0530

    Rendered the home page statically

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Render from "./components/Render/Render";
 
-import classes from "./index.css";
-
 const App = () => {
   return <Render />;
 };
--- src/components/Render/Render.js
+++ src/components/Render/Render.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Card from "../Card/Card";
+import Button from "../Button/Button";
+
+import "./Render.module.css";
 
 const Render = () => {
-
-}
+  return (
+    <div>
+      <h1>Quote Generator</h1>
+      <Card>
+        <Button />
+      </Card>
+    </div>
+  );
+};
 
 export default Render;
